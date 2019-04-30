bf9bbbf3d054c2583ffe237b4a25374be8b4b506
Sowayi <sapwao@gmail.com>
2019-04-30 07:18:19 -0400

    Add datatype to description property

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,5 @@
 export class Quote {
-    public showDescription;
+    public showDescription:boolean;
     constructor(public author:string, public description:string, public submitter:string, public like:number, public dislike:number, public publishDate:Date){
         this.showDescription = false;
     }
