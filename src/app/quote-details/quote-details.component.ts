ed9ca88e5eb3257136718255fa8223c5ad63d5a5
Sowayi <sapwao@gmail.com>
2019-04-30 07:20:23 -0400

    Fix button functionality within quote details

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,27 +1,29 @@
 import { Component, OnInit,Input,Output,EventEmitter} from '@angular/core';
-import {Quote} from "../quote";
+import {Quote} from '../quote';
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
-  @Input() quote:Quote;
-  @Output() removeQuote = new EventEmitter<boolean>();
-  @Output() likeQuote = new EventEmitter<boolean>();
-  @Output() dislikeQuote = new EventEmitter<boolean>();
+   @Input('quotes') quote:Quote;
+   @Output() removeQuote = new EventEmitter<boolean>();
+   @Output() addQuote = new EventEmitter();
+  //  @Output() likeQuote = new EventEmitter<boolean>();
+  //  @Output() dislikeQuote = new EventEmitter<boolean>();
   
-  quoteDelete(remove:boolean){
-      this.removeQuote.emit(remove);
+  Dislike(quote:Quote){
+      //this.dislike.emit(dislike);
+      quote.dislike +=1
     }
-  quoteLike(like:boolean){
-      this.likeQuote.emit(like);
+  Delete(remove:boolean){
+       this.removeQuote.emit(remove);
     }
-  quoteDislike(dislike:boolean){
-      this.dislikeQuote.emit(dislike);
+  Like(quote:Quote){
+    //  this.likeQuote.emit(like);
+    quote.like += 1
     }
   constructor() { }
   ngOnInit() {
   }
-
 }
